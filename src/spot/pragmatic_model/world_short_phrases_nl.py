4f88778cf65e6fff92e7e74c9408caa2182aad93
Jaap <j.m.kruijt@vu.nl>
2024-05-17 11:39:25 +0200

    add new characters to scenes for interactions 2 and 3

--- src/spot/pragmatic_model/world_short_phrases_nl.py
+++ src/spot/pragmatic_model/world_short_phrases_nl.py
@@ -22,7 +22,7 @@ ak11 = {'gender': ['vrouw', 'mevrouw'], 'hair_style': 'lang haar',
         'hair_color': ['donker haar', 'bruin haar'], 'hair_type': 'stijl', 'accessory': 'bril'}
 ak12 = {'gender': ['vrouw', 'mevrouw'], 'hair_style': 'bob',
         'hair_color': ['donker haar', 'bruin haar'], 'hair_type': 'golvend haar'}
-ak13 = {'gender': ['man', 'meneer'], 'hair_style': 'kort haar', 'hair_color': ['donker haar', 'bruin haar'], 'hair_type': 'krullen'}
+ak13 = {'gender': ['man', 'meneer'], 'hair_style': 'kaal'}
 ak14 = {'gender': ['man', 'meneer'], 'hair_style': 'netjes haar',
         'hair_color': ['donker haar', 'bruin haar'], 'hair_type': 'stijl', 'facial_hair': 'baard'}
 ak15 = {'gender': ['man', 'meneer'], 'hair_style': 'kort haar',
@@ -52,18 +52,44 @@ sp37 = {'gender': ['man', 'meneer'], 'hair_style': 'kort haar', 'hair_color': ['
 sp38 = {'gender': ['vrouw', 'mevrouw'], 'hair_style': ['halflang haar', 'lang haar'], 'hair_color': ['donker haar', 'bruin haar'], 'hair_type': 'stijl haar', 'accessory': 'oorbellen'}
 sp39 = {'gender': ['meisje'], 'age': ['jong', 'kind'], 'hair_color': 'donkerblond haar', 'hair_style': ['kort haar', 'bob'], 'hair_type': 'stijl'}
 sp40 = {'gender': ['vrouw', 'mevrouw'], 'hair_style': 'kort haar', 'hair_color': ['donkerblond haar', 'bruin haar'], 'hair_type': 'stijl'}
+sp41 = {'gender': ['man', 'meneer'], 'hair_style': 'kort haar', 'hair_color': 'blond haar', 'hair_type': 'golvend haar', 'accessory': 'bril'}
 
 
 ak_characters = {'1': ak1, '2': ak2, '3': ak3, '4': ak4, '5': ak5, '6': ak6, '7': ak7,
                  '8': ak8, '9': ak9, '10': ak10, '11': ak11, '12': ak12, '13': ak13, '14': ak14, '15': ak15}
+sp_characters = {'1': ak1, '2': ak2, '3': ak3, '4': ak4, '5': ak5, '6': ak6, '7': ak7,
+                 '8': ak8, '9': ak9, '10': ak10, '11': ak11, '12': ak12, '13': ak13, '14': ak14, '15': ak15,
+                 '16': sp16, '17': sp17, '18': sp18, '19': sp19, '20': sp20, '21': sp21, '22': sp22, '23': sp23,
+                 '24': sp24, '25': sp25, '26': sp26, '27': sp27, '28': sp28, '29': sp29, '30': sp30, '31': sp31,
+                 '32': sp32, '33': sp33, '34': sp34, '35': sp35, '36': sp36, '37': sp37, '38': sp38, '39': sp39,
+                 '40': sp40, '41': sp41}
 # ak_characters_rounds = {'1': [ak1, ak2, ak3, ak10, ak14], '2': [ak1, ak2, ak3, ak12, ak4], '3': [ak1, ak2, ak3, ak8, ak9],
 #                  '4': [ak1, ak2, ak3, ak6, ak15], '5': [ak1, ak2, ak3, ak13, ak11], '6': [ak1, ak2, ak3, ak7, ak5]}
 ak_robot_scene = {'1': {'1': '1', '2': '3', '3': '2', '10': '5', '14': '4'},
                   '2': {'1': '3', '2': '2', '3': '1', '12': '4', '4': '5'},
                   '3': {'1': '5', '2': '2', '3': '4', '8': '3', '9': '1'},
                   '4': {'1': '3', '2': '2', '3': '1', '6': '5', '15': '4'},
+                  '5': {'1': '5', '2': '2', '3': '4', '36': '1', '11': '3'},
+                  '6': {'1': '1', '2': '3', '3': '2', '7': '4', '5': '5'}}
+
+sp_robot_scene = {1: {'1': {'1': '1', '2': '3', '3': '2', '10': '5', '14': '4'},
+                  '2': {'1': '3', '2': '2', '3': '1', '12': '4', '4': '5'},
+                  '3': {'1': '5', '2': '2', '3': '4', '8': '3', '9': '1'},
+                  '4': {'1': '3', '2': '2', '3': '1', '6': '5', '15': '4'},
                   '5': {'1': '5', '2': '2', '3': '4', '13': '1', '11': '3'},
-                  '6': {'1': '1', '2': '3', '3': '2', '7': '4', '5': '4'}}
+                  '6': {'1': '1', '2': '3', '3': '2', '7': '4', '5': '4'}},
+                  2: {'1': {'1': '1', '2': '3', '3': '2', '31': '5', '30': '4'},
+                  '2': {'1': '3', '2': '2', '3': '1', '39': '4', '34': '5'},
+                  '3': {'1': '5', '2': '2', '3': '4', '38': '3', '13': '1'},
+                  '4': {'1': '3', '2': '2', '3': '1', '22': '5', '37': '4'},
+                  '5': {'1': '5', '2': '2', '3': '4', '17': '1', '41': '3'},
+                  '6': {'1': '1', '2': '3', '3': '2', '16': '4', '34': '5'}},
+                  3: {'1': {'1': '1', '2': '3', '3': '2', '24': '5', '25': '4'},
+                  '2': {'1': '3', '2': '2', '3': '1', '28': '4', '23': '5'},
+                  '3': {'1': '5', '2': '2', '3': '4', '19': '3', '21': '1'},
+                  '4': {'1': '3', '2': '2', '3': '1', '18': '5', '32': '4'},
+                  '5': {'1': '5', '2': '2', '3': '4', '40': '1', '26': '3'},
+                  '6': {'1': '1', '2': '3', '3': '2', '29': '4', '20': '5'}}}
 
 en1 = {'gender': 'man', 'hair_style': 'lang haar',
        'hair_color': 'bruin haar', 'hair_type': 'stijl', 'facial_hair': 'baard'}
